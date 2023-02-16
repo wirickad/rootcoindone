5fa03d4d204cd9ee0f1a21340afb5f5993d5c543
wirickad <92128478+wirickad@users.noreply.github.com>
2023-02-16 16:07:06 -0700

    Add project files.

---
+++ rootcoin/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+
+namespace rootcoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrivateKey key1 = new PrivateKey();
+            PublicKey wallet1 = key1.publicKey();
+
+            PrivateKey key2 = new PrivateKey();
+            PublicKey wallet2 = key2.publicKey();
+
+
+
+            Blockchain rootcoin = new Blockchain(2, 100);
+
+            Console.WriteLine("Start the miner");
+            rootcoin.MinePendingTransactions(wallet1);
+            Console.WriteLine("\nBalance of wallet1 is $" + rootcoin.GetBalanceOfWallet(wallet1).ToString());
+
+            Transaction tx1 = new Transaction(wallet1, wallet2, 10);
+            tx1.SignTransaction(key1);
+            rootcoin.addPendingTransaction(tx1);
+            Console.WriteLine("Start the miner");
+            rootcoin.MinePendingTransactions(wallet2);
+            Console.WriteLine("\nBalance of wallet1 is $" + rootcoin.GetBalanceOfWallet(wallet1).ToString());
+            Console.WriteLine("\nBalance of wallet2 is $" + rootcoin.GetBalanceOfWallet(wallet2).ToString());
+
+            string blockJSON = JsonConvert.SerializeObject(rootcoin, Formatting.Indented);
+            Console.WriteLine(blockJSON);
+
+            rootcoin.GetLatestBlock().PreviousHash = "12345";
+
+            if (rootcoin.IsChainValid())
+            {
+                Console.WriteLine("Blockchain is valid");
+            }
+            else
+            {
+                Console.WriteLine("Blockchain is NOT valid");
+            }
+        }
+    }
+}
+
+    
+
+
+    
